7573e3ca586e526eb8deab3ace0866e1f50fb3f0
Jeffery Roche <jkeauctions@gmail.com>
2020-02-28 14:17:42 -0500

    Updated for OpenShift deployment with env settings and requirements.txt

--- blog_project/wsgi.py
+++ blog_project/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+        'blog_project.settings.production')
 
 application = get_wsgi_application()
